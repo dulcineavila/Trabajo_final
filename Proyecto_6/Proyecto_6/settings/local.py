76346083e5c02623404b7a08e4a8d8276a7057c2
dulcineavila <dulcineavila@gmail.com>
2022-12-16 09:19:18 -0300

    -m base lista y primer modelo ya como tabla

--- Proyecto_6/Proyecto_6/settings/local.py
+++ Proyecto_6/Proyecto_6/settings/local.py
@@ -1,1 +1,11 @@
 from .base import *
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'podypoc18',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
--- Proyecto_6/blog/models.py
+++ Proyecto_6/blog/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class   Post (models.Model):
     title = models.CharField (max_length=200)
-    description = models.TextField()
+    description = models.TextField(null=True)
