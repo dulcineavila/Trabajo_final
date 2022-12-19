4d35a3a1e625f56a635d969f31b4b7eaa3eb7e79
dulcineavila <dulcineavila@gmail.com>
2022-12-19 10:45:14 -0300

    Modelo post completo

--- Proyecto_6/blog/models.py
+++ Proyecto_6/blog/models.py
@@ -1,7 +1,18 @@
 from django.db import models
-
+from django.conf import settings
+from django.utils import timezone
 # Create your models here.
 
 class   Post (models.Model):
+    autor = models.ForeignKey (settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True)
     title = models.CharField (max_length=200)
     description = models.TextField(null=True)
+    fecha_creacion= models.DateTimeField (default= timezone.now)
+    fecha_publicacion = models.DateTimeField (blank=True, null=True)
+
+    def publicar(self):
+        self.fecha_publicacion = timezone.now()
+        self.save()
+    
+    def __str__(self):
+        return self.title
