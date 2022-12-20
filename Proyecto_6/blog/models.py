851b466bc98e0016d5e608598047ae0e4f6b81be
dulcineavila <dulcineavila@gmail.com>
2022-12-20 11:31:36 -0300

    admin creado

--- Proyecto_6/blog/models.py
+++ Proyecto_6/blog/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from django.conf import settings
 from django.utils import timezone
 # Create your models here.
 
-class   Post (models.Model):
-    autor = models.ForeignKey (settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True)
+class Post(models.Model):
+    autor = models.CharField (max_length= 25)
     title = models.CharField (max_length=200)
     description = models.TextField(null=True)
     fecha_creacion= models.DateTimeField (default= timezone.now)
