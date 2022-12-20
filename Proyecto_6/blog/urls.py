b210375346d00e70d6a8b02ff67409dd9661552f
Mandibulin180 <gonzaloagustindelgado@gmail.com>
2022-12-20 16:01:02 -0300

    agregue urls,views,post en templates y forms

---
+++ Proyecto_6/blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('',views.Inicio,name='inicio'),
+    path('nosotros',views.Nosotros,name='nosotros'),
+    path('sumate',views.Sumate,name='sumate'),
+    path('publicaciones',views.Publicaciones,name='publicaciones'),
+    path('publicaciones/crear',views.Crear_posteo,name='Cpost'),
+    #path('publicaciones/editar/<str:tittle>', views.Editar_posteo,name='Epost'), llamar a un post para editarlo
+    path('eliminar/<str:tittle>', views.Borrar_posteo,name='Bpost'),
+    path('login', auth_views.LoginView.as_view(template_name='registrate.html') , name='login'),
+    path('logout', auth_views.logout_then_login, name='logout'),
+]
--- Proyecto_6/blog/views.py
+++ Proyecto_6/blog/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Post
+from .forms import Postform
 
-# Create your views here.
+# Create your views here
+
+def Inicio(request):
+    return render(request,'index.html')
+
+def Nosotros(request):
+    return render(request,'nosotros.html')
+
+def Publicaciones(request):
+    post=Post.objects.all()
+    return render(request,'post/publicaciones.html', {'Posteos':post})
+
+def Sumate(request):
+    return render(request,'sumate.html')
+
+def Crear_posteo(request):
+    formulario = Postform(request.POST or None, request.FILES or None)
+    if(formulario.is_valid()):
+        formulario.save()
+        return redirect('publicaciones')
+    return render(request, "Post/crear.html", {'formulario': formulario})
+
+def Editar_posteo(request,title):
+    post = Post.objects.get(title=title)
+    formulario = Postform(request.POST or None, request.FILES or None, instance=post)
+    if(formulario.is_valid() and request.POST):
+        formulario.save()
+        return redirect('publicaciones')
+    return render(request,'Post/editar.html', {'formulario':formulario})
+
+def Borrar_posteo(request,tittle):
+    post = Post.objects.get(tittle=tittle)
+    post.delete()
+    return redirect('publicaiones')
+
