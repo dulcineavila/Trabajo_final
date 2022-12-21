4757319f726ded784511ac2b155dca01364a19d9
Mandibulin180 <gonzaloagustindelgado@gmail.com>
2022-12-21 18:40:36 -0300

    Agrego cambios a urls

--- Proyecto_6/blog/urls.py
+++ Proyecto_6/blog/urls.py
@@ -8,12 +8,11 @@ urlpatterns = [
     path('nosotros',views.Nosotros,name='nosotros'),
     path('sumate',views.Sumate,name='sumate'),
     path('publicaciones',views.Publicaciones,name='publicaciones'),
-    path('publicaciones/crear',views.Crear_posteo,name='Cpost'),
-    path("publicaciones/editar/<str:title>", views.Editar_posteo,name='Epost'),
-    path("eliminar/<str:title>", views.Borrar_posteo,name='Bpost'),
+    path('posteos/crear',views.Crear_posteo,name='Cpost'),
+    path("posteos/editar/<slug:slug>", views.Editar_posteo,name='Epost'),
+    path("eliminar/<slug:slug>", views.Borrar_posteo,name='Bpost'),
     path('login', auth_views.LoginView.as_view(template_name='login.html') , name='login'),
     path('logout', auth_views.logout_then_login, name='logout'),
-    path('posteos',Posteo.as_view(), name='posteos'),
     path('<slug:slug>/', PostDetail.as_view(), name='postdetail'),
-    
+    path('posteos',Posteo.as_view(), name='posteos'),
 ]
